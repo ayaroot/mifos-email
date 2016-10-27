f444d797aaac7439aa1f28f9121fcd280441fb5f
awasum <yannickawasum@gmail.com>
2016-10-27 23:30:47 +0100

    Disable openssl checker for self generated ssl certificate from core

--- lib/mifosx-messenger/mifosx-helper.rb
+++ lib/mifosx-messenger/mifosx-helper.rb
@@ -11,7 +11,7 @@ module MifosXMessenger
 		@headers = nil
 
 		def initialize(options = {})
-			#OpenSSL::SSL::VERIFY_PEER == OpenSSL::SSL::VERIFY_NONE
+			OpenSSL::SSL::VERIFY_PEER == OpenSSL::SSL::VERIFY_NONE
 			@baseUrl = options['baseUrl'] || 'https://demo.openmf.org/fineract-provider-provider/api/v1'
 			uri = URI.parse(@baseUrl)
 			tenantId = options['tenantId'] || 'default'
@@ -19,7 +19,7 @@ module MifosXMessenger
 			pass = options['pass'] || 'password'
 			@http = Net::HTTP.new(uri.host, uri.port)
 			@http.use_ssl = uri.scheme == 'https'
-			#@http.verify_mode = OpenSSL::SSL::VERIFY_PEER
+			@http.verify_mode = OpenSSL::SSL::VERIFY_PEER
 			@headers = {
 				'Fineract-Platform-TenantId' => tenantId,
 				'Authorization' => 'Basic '+Base64.encode64(user+':'+pass).gsub("\n",'')
