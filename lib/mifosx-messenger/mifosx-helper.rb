389b1cb124c62fb59c0931089513c3d46d17175c
Yannick Awasum <yannickawasum@gmail.com>
2016-10-28 06:50:29 +0100

    Develop fix merge problems (#13)
    * Watch fineract changes, get entity url
    
    * Disable openssl checker for self generated ssl certificate from core
    
    * Get fixed deposit transactions
    
    * Listen to fixed deposit events
    
    * Get savings transactions
    
    * Fix to temp prob

--- lib/mifosx-messenger/mifosx-helper.rb
+++ lib/mifosx-messenger/mifosx-helper.rb
@@ -42,7 +42,47 @@ module MifosXMessenger
 			return nil
 		end
 
+		def get_client(clientId, options = {})
+			uri = get_entity_uri('clients', clientId)
+			get_entity(uri, options)
+		end
+		def get_client_email(clientId)
+			uri = get_entity_uri('email', clientId, 'https://localhost/fineract-provider/api/v1/datatables')
+			get_entity(uri)
+		end
+		def get_loan(loanId, options = {})
+			uri = get_entity_uri('loans', loanId)
+			get_entity(uri, options)
+		end
 
+		def get_loan_transaction(loanId, transId, options = {})
+			loan_uri = get_entity_uri('loans', loanId)
+			uri = get_entity_uri('transactions', transId, loan_uri)
+			get_entity(uri, options)
+		end
+
+		def get_savings(savingsId, options = {})
+			uri = get_entity_uri('savingsaccounts', savingsId)
+			get_entity(uri, options)
+		end
+
+		def get_savings_transaction(savingsId, transId, options = {})
+			savings_uri = get_entity_uri('savingsaccounts', savingsId)
+			uri = get_entity_uri('transactions', transId, savings_uri)
+			get_entity(uri, options)
+		end
+
+		def get_fixed_deposit(fixedDepositId, transId, options = {})
+			#fixed_deposit_uri = get_entity_uri('fixeddepositaccounts/template', fixedDepositId)
+			uri = get_entity_uri('fixeddepositaccounts/template', fixedDepositId)
+			get_entity(uri, options)
+		end
+
+		def get_fixed_deposit_transaction(fixedDepositId, transId, options = {})
+			fixed_deposit_uri = get_entity_uri('fixeddepositaccounts/template', savingsId)
+			uri = get_entity_uri('transactions', transId, fixed_deposit_uri)
+			get_entity(uri, options)
+		end
 
 	end
 end
